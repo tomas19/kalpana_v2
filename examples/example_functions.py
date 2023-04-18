f71e323d5a0ff4311fc19be5ee3a753b2e156d8e
tuck-themagic-brandon <bjtucker@ncsu.edu>
2023-04-18 09:24:02 -0400

    Beginning to redefine example functions, new ex3 colorbar

--- examples/example_functions.py
+++ examples/example_functions.py
@@ -1,80 +1,119 @@
-import pandas as pd
+import geopandas as gpd
 import numpy as np
+from numpy import linspace
 import netCDF4 as netcdf
-import matplotlib.pyplot as plt
+from shapely.geometry import Point, LineString
 import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.lines import Line2D
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.cm as cm
+import cmocean.cm as cmo
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-from shapely.geometry import Point, LineString
-import geopandas as gpd
-from matplotlib import colors
-import matplotlib.patches as mpatches
-import matplotlib.cm as cm
-from matplotlib.lines import Line2D
-from numpy import linspace
-import contextily as cxt
-import cmocean.cm as cmo
 
+## general plotting functions
 
-def plot_maxele(ncfile, levels):
+def plot_netcdf(nc, var, levels, xlims = None, ylims = None, 
+           ax = None, fig = None, fsize = (8, 6), cbar = False, 
+           cmap = 'viridis', cb_shrink = 1, cb_label = None,
+           ticks = None, background_map = True, point_circle = None):
+    ''' Funtion to create 2D plots from netcdf files. WIP
+            ## modification of plot2d from concorde
+        Parameters
+            nc: netcdf object
+                adcirc file already loaded to memory to plot as contours
+            var: string
+                name of the variable to plot. E.g. 'zeta', 'zeta_max'
+            levels: numpy array
+                contours to plot
+            xlims, ylims: list
+                limits of the plot
+            ax: matplotlib axis
+            fig: matplotlib figure
+            fsize: tuple
+                figure of the output size if fig and ax are not specified
+            cbar: boolean. Default False
+                True for adding colorbar
+            cmap: string
+                name of the cmap. For maxele viridis is recommended, but for fort.63 seismic works well
+            cb_shrink: float
+                useful to define size of colorbar
+            cb_label: string
+                colorbar label
+            ticks: list
+                colorbar ticks
+            background_map: boolean
+                True for using cartopy to plot a background map, doesn't work on the HPC
+            point_circle: shapely Point
+                draws a circle around the point
+        Returns
+            ax: matplotlib axes subplots
+    '''
     
-    # plot the maximum elevations of a given maxele.63.nc file
-    # functions extrapolated from plot2D_v2 from concorde
+    nc = netcdf.Dataset(nc, 'r')
+    if ax == None and background_map == False:
+        fig, ax = plt.subplots(figsize = fsize)
+    elif ax == None and background_map == True:
+        fig, ax = plt.subplots(figsize = fsize, subplot_kw={'projection': ccrs.PlateCarree()}, 
+                            constrained_layout=True)
+    
+    mycmap=plt.cm.get_cmap(cmap, int(levels[1]/levels[2])) 
+    if ticks is None:
+        ticks = np.arange(levels[0], levels[1], levels[2])
 
-    f1 = ncfile
-    nc1 = netcdf.Dataset(f1, 'r')
-    fig, ax = plt.subplots(figsize = (8, 4), nrows = 1, ncols = 2, subplot_kw={'projection': ccrs.PlateCarree()}, constrained_layout=True)
-
-
-    ##### subplot 0, 0
-
-    # extrapolated functions from plot2D_v2 in concorde
-    tri = mpl.tri.Triangulation(nc1['x'][:].data, nc1['y'][:].data, nc1['element'][:,:] - 1)
-    aux = nc1['zeta_max'][:].data
+    #create flood contours
+    tri = mpl.tri.Triangulation(nc['x'][:].data, nc['y'][:].data, nc['element'][:,:] - 1)
+    aux = nc[var][:].data
     aux = np.nan_to_num(aux, nan = -99999.0).reshape(-1)
-    contours = ax[0].tricontourf(tri, aux, levels = levels, cmap = 'viridis')
+    contours = ax.tricontourf(tri, aux, levels = np.arange(levels[0]-0.25, levels[1]+0.25, levels[2]), cmap = mycmap)
+    
+    if xlims is not None:
+        ax.set_xlim(xlims)
+    if ylims is not None:
+        ax.set_ylim(ylims)
+
+    if cbar:
+        cb = fig.colorbar(contours, shrink = cb_shrink, extend = 'both', ax = ax, fraction=0.046, pad=0.04, ticks = ticks)
+        cb.set_label(cb_label)
+    
+    if background_map == True:
+        # show coordinates and grid
+        gl = ax.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False, alpha=0.5, linestyle='--') 
+        gl.top_labels = False
+        gl.right_labels = False
+        ax.add_feature(cfeature.LAND)
+        ax.add_feature(cfeature.COASTLINE,lw=0.25)
+        ax.add_feature(cfeature.LAKES)
+
+    if point_circle is not None: # add circle
+        box = gpd.GeoDataFrame(geometry = [point_circle.buffer(6)])
+        box.boundary.plot(ax = ax, color = 'k', ls = '--')
+
+    return ax
+
+def plot_gdf():
+    return None
+
+
+## specific functions for example visualizations
+
+def plot_maxele(nc, levels):
+    
+    fig, ax = plt.subplots(figsize = (8,4), nrows = 1, ncols = 2,  subplot_kw={'projection': ccrs.PlateCarree()}, constrained_layout=True)
+    plot_netcdf(nc, 'zeta_max', levels, ax = ax[0], cbar = False, point_circle = Point(-76.8, 35.2))
     ax[0].set_xlabel('Longitude [deg]')
     ax[0].set_ylabel('Latitude [deg]')
-
-    # additional features of the plot
-    nc = Point((-76.8, 35.2))
-    box = gpd.GeoDataFrame(geometry = [nc.buffer(6)])
-    box.boundary.plot(ax = ax[0], color = 'k', ls = '--')
-    gl = ax[0].gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False, alpha=0.5, linestyle='--') # show coordinates and grig
-    gl.top_labels = False
-    gl.right_labels = False
-    ax[0].add_feature(cfeature.LAND)
-    ax[0].add_feature(cfeature.COASTLINE,lw=0.25)
-    ax[0].add_feature(cfeature.LAKES)
     ax[0].set_title('Full Domain')
-
-
-    ##### subplot 0, 1
-
-    # plot2D_v2
-    tri = mpl.tri.Triangulation(nc1['x'][:].data, nc1['y'][:].data, nc1['element'][:,:] - 1)
-    aux = nc1['zeta_max'][:].data
-    aux = np.nan_to_num(aux, nan = -99999.0).reshape(-1)
-    #print(aux)
-    contours = ax[1].tricontourf(tri, aux, levels = levels, cmap = 'viridis')
+    plot_netcdf(nc, 'zeta_max', levels, xlims = [-78.5, -75], ylims = [33.5, 37], ax = ax[1], fig = fig, cbar = True, cb_label = 'Max water level [m MSL]', ticks = np.arange(levels[0]-0.25, levels[1]+0.25, levels[2]), point_circle = Point(-76.8, 35.2))
     ax[1].set_xlabel('Longitude [deg]')
     ax[1].set_ylabel('Latitude [deg]')
-    ax[1].set_xlim([-78.5, -75])
-    ax[1].set_ylim([33.5, 37])
-    cb = fig.colorbar(contours, extend = 'both', ax = ax[1], fraction=0.046, pad=0.04)
-    cb.set_label('Max water level [m MSL]')
-
-    # additional
-    ax[1].add_feature(cfeature.LAND)
-    ax[1].add_feature(cfeature.COASTLINE,lw=0.25)
-    ax[1].add_feature(cfeature.LAKES)
-    gl = ax[1].gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False, alpha=0.5, linestyle='--') # show coordinates and grig
-    gl.top_labels = False
-    gl.right_labels = False
     ax[1].set_title('North Carolina')
+    fig.suptitle('Maximum Water Levels (Florence)', fontsize = 16)
 
-    fig.suptitle(f'Maximum Water Levels (Florence)', fontsize = 16)
+
+
 
 def plot_polylines(gdf, levels):
     fig, ax = plt.subplots(figsize = (9, 4.5), nrows = 1, ncols = 2, subplot_kw={'projection': ccrs.PlateCarree()}, constrained_layout=True)
@@ -137,8 +176,10 @@ def plot_polylines(gdf, levels):
 
     fig.suptitle(f'Polyline Contours created from nc2shp()', fontsize = 16)
 
-def plot_polygons(gdf):
+def plot_polygons(gdf, levels):
     fig, ax = plt.subplots(figsize = (8, 4), nrows = 1, ncols = 2, subplot_kw={'projection': ccrs.PlateCarree()}, constrained_layout=True)
+    cmap=plt.cm.get_cmap('viridis', int(levels[1]/levels[2])) 
+    ticks = np.arange(levels[0], levels[1], levels[2])
 
     ##### subplot 0, 0
 
@@ -181,9 +222,8 @@ def plot_polygons(gdf):
     gl.right_labels = False
     ax[1].set_title('North Carolina')
 
-
-    gdf.plot(column = 'zMean', legend = True, cmap = 'viridis', vmin = -0.25, vmax = 2.75,
-            legend_kwds={'label': 'Max water level [m MSL]', 'orientation': 'vertical', 'fraction': 0.046, 'pad': 0.04}, 
+    gdf.plot(column = 'zMean', legend = True, cmap = cmap, vmin = levels[0]-0.25, vmax = levels[1]-0.25,
+            legend_kwds={'label': 'Max water level [m MSL]', 'orientation': 'vertical', 'fraction': 0.046, 'pad': 0.04, 'ticks': ticks}, 
             ax = ax[1], aspect = 'equal')
 
     fig.suptitle(f'Polygon Contours created from nc2shp()', fontsize = 16)
@@ -195,6 +235,8 @@ def polygon_compare(ncfile, levels, gdf):
     f1 = ncfile
     nc1 = netcdf.Dataset(f1, 'r')
     fig, ax = plt.subplots(figsize = (9, 4.5), nrows = 1, ncols = 2, subplot_kw={'projection': ccrs.PlateCarree()}, constrained_layout=True)
+    cmap=plt.cm.get_cmap('viridis', int(levels[1]/levels[2])) 
+    ticks = np.arange(levels[0], levels[1], levels[2])
 
     ##### subplot 0, 0
 
@@ -202,12 +244,12 @@ def polygon_compare(ncfile, levels, gdf):
     tri = mpl.tri.Triangulation(nc1['x'][:].data, nc1['y'][:].data, nc1['element'][:,:] - 1)
     aux = nc1['zeta_max'][:].data
     aux = np.nan_to_num(aux, nan = -99999.0).reshape(-1)
-    contours = ax[0].tricontourf(tri, aux, levels = levels, cmap = 'viridis')
+    contours = ax[0].tricontourf(tri, aux, levels = np.arange(levels[0]-0.25, levels[1]+0.25, levels[2]), cmap = cmap)
     ax[0].set_xlabel('Longitude [deg]')
     ax[0].set_ylabel('Latitude [deg]')
     ax[0].set_xlim([-78.5, -75])
     ax[0].set_ylim([33.5, 37])
-    cb = fig.colorbar(contours, extend = 'both', ax = ax[0], fraction=0.046, pad=0.04)
+    cb = fig.colorbar(contours, extend = 'both', ax = ax[0], fraction=0.046, pad=0.04, ticks = ticks)
     cb.set_label('Max water level [m MSL]')
 
     # additional
@@ -237,8 +279,8 @@ def polygon_compare(ncfile, levels, gdf):
     gl.right_labels = False
     ax[1].set_title('Polygon Contours from nc2shp()')
 
-    gdf.plot(column = 'zMean', legend = True, cmap = 'viridis', vmin = -0.25, vmax = 2.75,
-            legend_kwds={'label': 'Max water level [m MSL]', 'orientation': 'vertical', 'fraction': 0.046, 'pad': 0.04}, 
+    gdf.plot(column = 'zMean', legend = True, cmap = cmap, vmin = levels[0]-0.25, vmax = levels[1]-0.25,
+            legend_kwds={'label': 'Max water level [m MSL]', 'orientation': 'vertical', 'fraction': 0.046, 'pad': 0.04, 'ticks': ticks}, 
             ax = ax[1], aspect = 'equal')
 
 def polyline_compare(ncfile, levels, gdf, lev):
@@ -386,4 +428,3 @@ def mesh_plot(gdf, column, bounds, title, cmap = None):
         mygdf.plot(column = column, legend = True, ax = ax, aspect = 'equal', cmap = 'viridis', edgecolor='black', linewidth = 0.2,
                 legend_kwds={'label': 'Element Area [square km]', 'orientation': 'vertical', 'fraction': 0.046, 'pad': 0.04},
                 )
-
