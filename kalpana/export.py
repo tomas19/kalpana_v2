3d010a2095b42de55e0373557fe69d3c1e013ca6
James McManus <jmpmcman@renci.org>
2023-08-07 14:43:42 -0400

    Updated from main

--- kalpana/export.py
+++ kalpana/export.py
@@ -888,45 +888,31 @@ def fort14togdf(filein, epsgIn, epsgOut, fileintype='netcdf'):
                 coordinate system of the adcirc input
             epsgOut: int
                 coordinate system of the output shapefile
-            fileInType: str
-                input file type, which are netcdf (default) or fort.14     
+            fileintype: str
+                file type used to get nodes and elements. Default netcdf
         Returns
             gdf: GeoDataFrame
                 GeoDataFrame with polygons as geometry and more info such as: area, representative
                 element size, centroids coordinates, and vertices
     '''
     ## read only the two first lines of the file to get the number of elements and nodes
-    '''
-    with open(filein) as fin:
-        head = list(islice(fin, 2))
-        data = [int(x) for x in head[1].split()]
-    ## read nodes
-    nodes = np.loadtxt(filein, skiprows = 2, max_rows = data[1], usecols = (1, 2, 3))
-    ## read elements
-    elem = np.loadtxt(filein, skiprows = 2 + data[1], max_rows = data[0], usecols = (2, 3, 4)) - 1
-    x = nodes[:, 0]
-    y = nodes[:, 1]
-    z = nodes[:, 2]
-    '''
-    if fileintype == 'fort.14':                                                                                                                                                                               
-        ## read only the two first lines of the file to get the number of elements and nodes                                                                                                                  
-        with open(filein) as fin:                                                                                                                                                                             
-            head = list(islice(fin, 2))                                                                                                                                                                       
-            data = [int(x) for x in head[1].split()]                                                                                                                                                          
-        ## read nodes                                                                                                                                                                                         
-        nodes = np.loadtxt(filein, skiprows = 2, max_rows = data[1], usecols = (1, 2, 3))                                                                                                                     
-        ## read elements                                                                                                                                                                                      
-        elem = np.loadtxt(filein, skiprows = 2 + data[1], max_rows = data[0], usecols = (2, 3, 4)) - 1                                                                                                        
-        x = nodes[:, 0]                                                                                                                                                                                       
-        y = nodes[:, 1]                                                                                                                                                                                       
-        z = nodes[:, 2]                                                                                                                                                                                       
-    elif fileintype == 'netcdf':                                                                                                                                                                              
-        with netcdf.Dataset(filein) as nc:                                                                                                                                                                    
-            x = nc['x'][:].data                                                                                                                                                                               
-            y = nc['y'][:].data                                                                                                                                                                               
-            z = nc['depth'][:].data                                                                                                                                                                           
-            elem = nc['element'][:].data - 1  
-	    
+    if fileintype == 'fort.14':
+        with open(filein) as fin:
+            head = list(islice(fin, 2))
+            data = [int(x) for x in head[1].split()]
+        ## read nodes
+        nodes = np.loadtxt(filein, skiprows = 2, max_rows = data[1], usecols = (1, 2, 3))
+        ## read elements
+        elem = np.loadtxt(filein, skiprows = 2 + data[1], max_rows = data[0], usecols = (2, 3, 4)) - 1
+        x = nodes[:, 0]
+        y = nodes[:, 1]
+        z = nodes[:, 2]
+    elif fileintype == 'netcdf':
+        with netcdf.Dataset(filein) as nc:
+            x = nc['x'][:].data              
+            y = nc['y'][:].data         
+            z = nc['depth'][:].data        
+            elem = nc['element'][:].data - 1
     ## matplotlib triangulation
     tri = mpl.tri.Triangulation(x, y, elem)
     ## select the coordinate of each vertex
