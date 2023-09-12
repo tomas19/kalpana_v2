f4abd343af2ce3ea259a10b6115e94b17f27520a
tomas19 <tomascuevas@gmail.com>
2023-09-12 14:21:24 -0400

    readme

--- examples/downscaling_example04.py
+++ examples/downscaling_example04.py
@@ -4,18 +4,39 @@ from downscalingHeadLoss import preCompCostSurface
 from loguru import logger
 
 # %%
+## grass version
 grassVer = 8.2
-createGrassLocation = True
-pathGrassLocation = r'/mnt/drive1/Insyncs/NCSU/Kalpana/Debug/headLoss01_Coned10m_r1'
-pathRasFiles = r'/mnt/drive1/Insyncs/NCSU/Kalpana/Data/topo'
-rasterFiles = r'NC_CoNED_res10m.tif'
-manningRasPath = r"/mnt/drive1/Insyncs/NCSU/Kalpana/Data/manning/NLCD_2016_Land_Cover_L48_20190424.img"
-manningLandCover = r"/home/tacuevas/github/Kalpana/adds/manning/landCover_manning.txt"
-epsg = 6346
-pathOutRawCostRas = os.path.join(pathGrassLocation, 'rawCostRaster_NCconed10m.tif')
-pathOutTotalCostRas = os.path.join(pathGrassLocation, 'totalCostRaster_NCconed10m.tif')
-pathOutDownDemCorr = os.path.join(pathGrassLocation, 'downDemCorr_NCconed10m.tif')
 
+## boolean flag for creating a GRASS GIS location
+createGrassLocation = True
+
+## path to the grass location
+pathGrassLocation = r'/mnt/drive1/Insyncs/NCSU/Kalpana/Debug/headLoss01_Coned10m_r1'
+
+## path of the downscaling raster
+pathRasFiles = r'/mnt/drive1/Insyncs/NCSU/Kalpana/Data/topo'
+
+## downscaling raster file name
+rasterFiles = r'NC_CoNED_res10m.tif'
+
+## full path of the land cover DEM
+manningRasPath = r"/mnt/drive1/Insyncs/NCSU/Kalpana/Data/manning/NLCD_2016_Land_Cover_L48_20190424.img"
+
+## rules list to convert from land cover to manning
+manningLandCover = r"/home/tacuevas/github/Kalpana/adds/manning/landCover_manning.txt"
+
+## coordinate reference system 
+epsg = 6346
+
+## full path of the output dem with the raw cost
+pathOutRawCostRas = os.path.join(pathGrassLocation, 'rawCostRaster_NCconed10m.tif')
+
+## full path of the output dem with the total cost
+pathOutTotalCostRas = os.path.join(pathGrassLocation, 'totalCostRaster_NCconed10m.tif')
+
+## full path of the output downscaling DEM. In case the downscaling DEM does not have bathy,
+## cells with water land cover class are set to bathymetry 0 so water cells are included in the calculations.
+pathOutDownDemCorr = os.path.join(pathGrassLocation, 'downDemCorr_NCconed10m.tif')
 
 
 preCompCostSurface(grassVer, createGrassLocation, pathGrassLocation, pathRasFiles, rasterFiles, epsg, manningRasPath, manningLandCover, 
